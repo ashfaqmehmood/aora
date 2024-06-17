46ce3047219058d3d5104696c8f4047ba8393516
Ashfaq Mehmood <ashfaqmehmood.01@gmail.com>
2024-06-17 18:43:48 +0530

    init

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,49 @@
+import { View, Text, Image } from "react-native";
+import { Tabs, Redirect } from "expo-router";
+import React from "react";
+
+import { icons } from "../../constants";
+
+const TabIcon = ( icon, color, name, focused) => {
+  return (
+    <View>
+      <Image 
+        source={icon}
+        resizeMode="contain"
+        tintColor={color}
+        className="w-6 h-6"        
+      />
+      <Text className>
+        {name}
+      </Text>
+    </View>
+  );
+
+}
+
+const TabsLayout = () => {
+  return (
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name="home"
+          options={{ 
+            title: "Home", 
+            headerShown: false, 
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcon
+                icon={icons.home}
+                color={color}
+                name="Home"
+                focused={focused}              
+              />
+            ) 
+          }}
+        />
+        <Tabs.Screen name="profile" options={{ title: "Profile" }} />
+      </Tabs>
+    </>
+  );
+};
+
+export default TabsLayout;
