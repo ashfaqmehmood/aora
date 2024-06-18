ce8c7a57e1e60de382b197fac7749bf96393d45f
Ashfaq Mehmood <ashfaqmehmood.01@gmail.com>
2024-06-19 00:20:08 +0530

    refactor: Improve VideoCard component layout and styling

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,6 +1,8 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 
+import { icons } from "../constants";
+
 const VideoCard = ({
   video: {
     title,
@@ -11,22 +13,30 @@ const VideoCard = ({
 }) => {
   return (
     <View className="flex-col items-center px-4 mb-14">
-      <View>
-        <Image
-          source={{ uri: thumbnail }}
-          className="w-full h-44 rounded-2xl"
-          resizeMode="cover"
-        />
-        <View className="flex-row items-center mt-2">
-          <Image
-            source={{ uri: avatar }}
-            className="w-8 h-8 rounded-full"
-            resizeMode="cover"
-          />
-          <Text className="text-sm text-gray-100 ml-2">{username}</Text>
+      <View className="flex-row gap-3 items-start">
+        <View className="justify-center items-center flex-row flex-1">
+          <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
+            <Image 
+                source={{ uri: avatar }}
+                className="w-full h-full rounded-lg"
+                resizeMode="cover"
+            />
+          </View>
+
+          <View className="justify-center flex-1 ml-3 gap-y-1">
+            <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
+                {title}
+            </Text>
+            <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
+                {username}
+            </Text>
+          </View>
+        </View>
+
+        <View className="pt-2">
+            <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
         </View>
       </View>
-      <Text className="text-2xl  text-white">{title}</Text>
     </View>
   );
 };
