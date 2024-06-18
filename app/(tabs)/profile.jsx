9a46ce678df16457b964712fa5b6a6371a55705e
Ashfaq Mehmood <ashfaqmehmood.01@gmail.com>
2024-06-19 04:51:34 +0530

    a

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import VideoCard from "../../components/VideoCard";
+import { getUserPosts, searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import { useLocalSearchParams } from "expo-router";
+
+import { useGlobalContext } from "../../context/GlobalProvider"
 
 const Profile = () => {
+  const { user, setUser, setIsLoggedIn } = useGlobalContext();
+  const { data: posts, refetch } = useAppwrite(() => getUserPosts(user.$id));
+
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
-  )
-}
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        // data={[]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Search Results
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">
+              
+            </Text>
 
-export default Profile
+            <View className="mt-6 mb-8">
+              {/* <SearchInput initialQuery={query} /> */}
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default Profile;
