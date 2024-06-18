65099fce4a4de04f562a6e4dfdb832ffcf5cb487
Ashfaq Mehmood <ashfaqmehmood.01@gmail.com>
2024-06-19 00:20:26 +0530

    refactor: Improve VideoCard component layout and styling

--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -16,25 +16,31 @@ const VideoCard = ({
       <View className="flex-row gap-3 items-start">
         <View className="justify-center items-center flex-row flex-1">
           <View className="w-[46px] h-[46px] rounded-lg border border-secondary justify-center items-center p-0.5">
-            <Image 
-                source={{ uri: avatar }}
-                className="w-full h-full rounded-lg"
-                resizeMode="cover"
+            <Image
+              source={{ uri: avatar }}
+              className="w-full h-full rounded-lg"
+              resizeMode="cover"
             />
           </View>
 
           <View className="justify-center flex-1 ml-3 gap-y-1">
-            <Text className="text-white font-psemibold text-sm" numberOfLines={1}>
-                {title}
+            <Text
+              className="text-white font-psemibold text-sm"
+              numberOfLines={1}
+            >
+              {title}
             </Text>
-            <Text className="text-xs text-gray-100 font-pregular" numberOfLines={1}>
-                {username}
+            <Text
+              className="text-xs text-gray-100 font-pregular"
+              numberOfLines={1}
+            >
+              {username}
             </Text>
           </View>
         </View>
 
         <View className="pt-2">
-            <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
+          <Image source={icons.menu} className="w-5 h-5" resizeMode="contain" />
         </View>
       </View>
     </View>
